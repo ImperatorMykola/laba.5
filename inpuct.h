01802868e699e40b6d2a650e5d4292fa577785c3
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-06-12 07:53:20 +0300

    create structure of tree

--- inpuct.h
+++ inpuct.h
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <math.h>
+#include <unordered_map>
+using namespace std;
+typedef std::unordered_map<std::string, double>  hash_map;
 
+struct three
+{
+  string od;
+  double R;
+  three* left;
+  three* right;
+  void operator=(three p)
+  {
+    od = p.od;
+    R = p.R;
+    left = p.left;
+    right = p.right;
+  }
+  three()
+  {
+    od = " ";
+    R = 1000000;
+  }
+};
