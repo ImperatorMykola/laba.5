914eadaaba2b6afe583965770f5606d950d9503a
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-06-12 08:06:27 +0300

    start fill it

--- inpuct.h
+++ inpuct.h
@@ -34,9 +34,27 @@ class inpact
   hash_map pl;
 public:
   inpact (string t);
-  /*three plut(three, three, string);
+  three plut(three, three, string);
   int GetPriority(string);
   void buildtree();
   double calculate();
-  double carculate(three*);*/
+  double carculate(three*);
 };
+
+inpact::inpact(string t)
+{
+  ifstream fic(t);
+  string h, g;
+  double a;
+  while (!fic.eof())
+  {
+    getline(fic, h, '=');
+    if (h[h.size() - 1] == ';')
+      break;
+    getline(fic, g, '\n');
+    a = stod(g);
+    pl.insert(pair <string, double> (h, a));
+  }
+  q = h.substr(0, h.size() - 1);
+  buildtree();
+}
