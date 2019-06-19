1116fdbea2f327d58148025d08a5e537474e0914
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-06-19 14:44:00 +0300

    в главную функцию добавлен объект класса и использование его методов

--- Proga.cpp
+++ Proga.cpp
@@ -1,10 +1,14 @@
-
 #include <iostream>
 #include <string.h>
 #include "inpact.h"
+#include <conio.h>
 using namespace std;
 
 int main()
 {
-    return 0;
+	inpact r("input.txt");
+	double q = r.calculate();
+	cout << endl << "q =  " << q << endl;
+	_getch();
+	return 0;
 }
